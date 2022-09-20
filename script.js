b469bdb85ae7910a00bc1436be2ce3dc8a2ffd91
Max <mushbit@protonmail.com>
2022-09-20 09:06:00 +0200

    Added query selectors to all button elements and the input

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnOne = document.querySelector('button.one');
+const btnTwo = document.querySelector('button.two');
+const btnThree = document.querySelector('button.three');
+const btnFour = document.querySelector('button.four');
+const btnFive = document.querySelector('button.five');
+const btnSix = document.querySelector('button.six');
+const btnSeven = document.querySelector('button.seven');
+const btnEight = document.querySelector('button.eight');
+const btnNine = document.querySelector('button.nine');
+const btnZero = document.querySelector('button.zero');
+
+const btnAdd = document.querySelector('button.add');
+const btnSubtract = document.querySelector('button.subtract');
+const btnMultiply = document.querySelector('button.multiply');
+const btnDivide = document.querySelector('button.divide');
+
+const input = document.querySelector('input.input')
+const btnSubmit = document.querySelector('button.submit')
