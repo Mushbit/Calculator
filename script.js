12d6cbc8ed39e7068882ad32a3ca83129bf5a5cf
Max <mushbit@protonmail.com>
2022-10-20 10:30:41 +0200

    Working on operate function.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myEx = ['1', '/', '2', '-', '5', '8', '*', '3', '-', '3'];
+
 function add(a, b) {
     return a + b;
 }
@@ -14,6 +16,53 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate() {
-    
+function findAddSub(arr) {
+    return arr.findIndex(el => el === '+' || el === '-')
+}
+
+function findMulDiv(arr) {
+    return arr.findIndex(el => el === '*' || el === '/')
+}
+
+function operate(arr) {
+    let result = 0;
+    while (arr.includes( '*' || '/' )) {
+        
+    }
+
+    while (arr.includes( '+' || '-' )) {
+
+    }
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr.includes('*')) {
+
+            result = multiply(+arr[i-1], +arr[i+1]);
+            arr.splice(i-1, 3, result);
+
+        } else if (arr.includes('/')) {
+
+            result = divide(+arr[i-1], +arr[i+1]);
+            arr.splice(i-1, 3, result);
+
+        }
+
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr.includes('+')) {
+
+            result = add(+arr[i-1], +arr[i+1]);
+            arr.splice(i-1, 3, result);
+
+        } else if (arr.includes('-')) {
+
+            result = subtract(+arr[i-1], +arr[i+1]);
+            arr.splice(i-1, 3, result);
+
+        }
+
+    }
+
 }
